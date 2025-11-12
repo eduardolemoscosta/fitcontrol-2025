29e2ffe92f5306f1a3744c83c2b4fc842fea96fb
eduardolemoscosta <eduardolemoscosta123@gmail.com>
2025-11-12 15:39:54 -0300

    fazendo ajustes

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "C:\Users\20221181110033\Desktop\apoo\fitcontrol_django\venv"
+set -gx VIRTUAL_ENV "C:\Users\20221181110033\Desktop\eduardo projeto\fitcontrol-2025\venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/Scripts" $PATH
